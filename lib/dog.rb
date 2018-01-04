9e9681bc8f676b3b86134a3671526c5045ec1fb3
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-04 19:56:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,17 @@
 class Dog
 
-  attr_reader :name 
+  attr_reader :name
 
   @@all = []
- 
+
   def initialize(name)
     @name = name
   end
-  
-  
+
+  def self.all
+    @@all.each do |dog|
+      puts dog
+    end
+  end
+
 end
